990053dabb20d2cbd4dea6152ee2fe6f0200b504
degausser <degausser02@outlook.com>
2023-06-08 10:54:37 -0500

    completed exercises

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ console.log(
 
 // Exercise 1
 
+const firstName;
+let lastName; 
+var age;
+
 // Exercise 2
 
+firstName = "Charlie";
+lastName = "Doyle";
+age = 28;
+
 // Exercise 3
+
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
